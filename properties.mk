3f78b6eb354c266ad5649db1e5458e13b14a9bdc
LuK1337 <priv.luk@gmail.com>
2022-09-02 14:14:28 +0200

    sm7250-common: Pull appropriate USB props from vendor_product.mk
    Change-Id: I93b25c5d0989c83d1c8d7b7946a083771febdddd

--- properties.mk
+++ properties.mk
@@ -387,7 +387,13 @@ ro.soc.model=SM7250
 # USB
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.usb.diag.func.name=diag \
-vendor.usb.use_ffs_mtp=0
+vendor.usb.dpl.inst.name=dpl \
+vendor.usb.qdss.inst.name=qdss \
+vendor.usb.rmnet.func.name=gsi \
+vendor.usb.rmnet.inst.name=rmnet \
+vendor.usb.rndis.func.name=gsi \
+vendor.usb.use_ffs_mtp=0 \
+vendor.usb.use_gadget_hal=0
 
 # Volume steps
 PRODUCT_PROPERTY_OVERRIDES += \
